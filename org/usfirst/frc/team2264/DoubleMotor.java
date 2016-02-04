898651c9814c8414f7533442c20c5ddee3973343
pidatpre000 <pidatpre000@isd284.com>
2016-02-03 18:40:19 -0600

    finished double motor!

--- org/usfirst/frc/team2264/DoubleMotor.java
+++ org/usfirst/frc/team2264/DoubleMotor.java
@@ -1,9 +1,6 @@
 package org.usfirst.frc.team2264;
 
-import org.usfirst.frc.team2264.robot.RobotMap;
-
 import edu.wpi.first.wpilibj.CANTalon;
-import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SpeedController;
 
 public class DoubleMotor implements SpeedController {
@@ -18,44 +15,44 @@ public class DoubleMotor implements SpeedController {
 
 	@Override
 	public void pidWrite(double output) {
-		this.pidWrite(get());	
+		this.front.pidWrite(output);
+		this.rear.pidWrite(output);	
 	}
 
 	@Override
 	public double get() {
-		// TODO Auto-generated method stub
-		return 0;
+		return front.getSpeed();
+	
 	}
 
 	@Override
 	public void set(double speed, byte syncGroup) {
-		// TODO Auto-generated method stub
-		
-
+		this.front.set(speed, syncGroup);
+		this.rear.set(speed, syncGroup);
 	}
 
 	@Override
 	public void set(double speed) {
-		// TODO Auto-generated method stub
-
+		this.front.set(speed);
+		this.rear.set(speed);
 	}
 
 	@Override
 	public void setInverted(boolean isInverted) {
-		// TODO Auto-generated method stub
-
+		this.front.setInverted(isInverted);
+		this.rear.setInverted(isInverted);
 	}
 
 	@Override
 	public boolean getInverted() {
-		// TODO Auto-generated method stub
-		return false;
+		return front.getInverted();
+		
 	}
 
 	@Override
 	public void disable() {
-		// TODO Auto-generated method stub
-
+		this.front.disable();
+		this.rear.disable();
 	}
 
 }
