5eb34b20a5618179b6cc320175e32e0adcee59a2
Kushal Sehgal <sehgakus000@isd284.com>
2016-02-03 19:55:58 -0600

    Winch class added.

--- org/usfirst/frc/team2264/Winch.java
+++ org/usfirst/frc/team2264/Winch.java
@@ -1,5 +1,47 @@
 package org.usfirst.frc.team2264;
 
-public class Winch {
+import edu.wpi.first.wpilibj.CANTalon;
 
+public class Winch {
+	CANTalon winder;
+	long startTime;
+	long movingTime;
+	long lastUpdate;
+	final long oneSecond = (1000000000);
+	public Winch(CANTalon winder){
+		this.winder = winder;
+	}
+	public void set(double speed){
+		this.winder.set(speed);
+	}
+	public void stopWinding(){
+		this.set(0);
+	}
+	public void startWinding(double speed){
+		
+		if (speed > 0){
+			this.set(speed);
+		}
+	}
+	public void startUnwinding(double speed){
+		if (speed < 0){
+			this.set(speed);
+		}
+	}
+	public void Update(){
+		long currentTime = System.nanoTime();
+		long elapsedTime = (currentTime - this.lastUpdate);
+		
+		if(this.winder.get() > 0){
+			this.movingTime += elapsedTime;
+		}
+		else if (this.winder.get() <0){
+			this.movingTime -= elapsedTime;
+		}
+		
+		if ((this.movingTime >= (5*this.oneSecond)) || (this.movingTime <= 0)){
+			this.stopWinding();
+		}
+		this.lastUpdate = currentTime;
+	}
 }
