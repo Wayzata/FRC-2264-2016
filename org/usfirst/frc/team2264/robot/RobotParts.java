2d3b841d2a3e1adbc0a6d2b7a4dccc0f508851eb
Mariel <mariel21279@gmail.com>
2016-02-11 17:37:01 -0600

    Creates shooter code + some robot parts

--- org/usfirst/frc/team2264/robot/RobotParts.java
+++ org/usfirst/frc/team2264/robot/RobotParts.java
@@ -8,6 +8,12 @@ public class RobotParts {
 		public static final int RIGHT_REAR = 19;
 		public static final int SLIDE = 26;
 		
+		public static final int SHOULDER = 24;
+		public static final int ELBOW = 25;
+		
+		public static final int SHOOTER_BELT = 21;
+		public static final int SHOOTER_ARM = 23;
+		public static final int SHOOTER_MAIN = 22;
 	}
 	public static class SWITCHES {
 		public static final int EXETEND = 0;
--- org/usfirst/frc/team2264/robot/subsystems/Shooter.java
+++ org/usfirst/frc/team2264/robot/subsystems/Shooter.java
@@ -1,8 +1,78 @@
 package org.usfirst.frc.team2264.robot.subsystems;
+ 
+import edu.wpi.first.wpilibj.CANTalon;
+import org.usfirst.frc.team2264.robot.RobotParts;
 
 /**
- * @author Gwyneth and Mariel
+ * The controlling class for the Shooter
+ * @author mariel
  */
-public class Shooter {
+ public class Shooter {
 
-}
+	
+
+	private CANTalon mainMotor;
+	private CANTalon beltMotor;
+	private CANTalon armMotor;
+
+	/**
+	 * Constructs the various shooter motors
+	 */
+	public Shooter() {
+		this.mainMotor = new CANTalon(RobotParts.MOTORS.SHOOTER_MAIN);
+		this.beltMotor = new CANTalon(RobotParts.MOTORS.SHOOTER_BELT);
+		this.armMotor = new CANTalon(RobotParts.MOTORS.SHOOTER_ARM);
+	}
+
+	/**
+	 * raises the arm
+	 */
+	public void raiseArm() {
+		this.armMotor.set(1);
+	}
+
+	/**
+	 * lowers the arm
+	 */
+	public void lowerArm() {
+		this.armMotor.set(-1);
+	}
+
+	/**
+	 * Pauses and stops the arm from moving
+	 */
+	public void stopArm() {
+		this.armMotor.set(0);
+	}
+
+	/**
+	 * This either goes counter cw or ccw, i am unsure
+	 */
+	public void pullBelt() {
+		this.beltMotor.set(1);
+	}
+
+	/**
+	 * This either goes counter cw or ccw, i am unsure
+	 */
+	public void pushBelt() {
+		this.beltMotor.set(-1);
+	}
+
+	/**
+	 * pauses the belt motor
+	 */
+	public void stopBelt() {
+	this.beltMotor.set(0);
+	}
+
+	/**
+	 * main motor in robot shooter spins and shoots the ball
+	 */
+ 	public boolean shoot() {
+
+		this.mainMotor.set(1);
+
+ 		// TODO
+ 		return false;
+ 	}
